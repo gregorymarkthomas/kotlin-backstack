e0d1ce943c28b1f6b012d58df6ab5d8711684a95
Gregory Mark Thomas <gregory@gregorymarkthomas.com>
2024-02-13 14:28:17 +0000

    Added a library for instrumented tests, wrote some instrumented tests to test what happens upon a screen orientation change - the result of viewsRemainAfterOrientationChange() feel like a false-positive though, compared to real-world result - needs work.

---
+++ backstackexample/src/androidTest/java/com/gregorymarkthomas/backstackexample/MainActivityTest.kt
@@ -0,0 +1,106 @@
+package com.gregorymarkthomas.backstackexample
+
+import android.content.pm.ActivityInfo
+import android.os.Handler
+import androidx.test.core.app.launchActivity
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import com.gregorymarkthomas.backstack.view.BackstackActivity
+import com.gregorymarkthomas.backstackexample.view.AView
+import com.gregorymarkthomas.backstackexample.view.BView
+import com.gregorymarkthomas.backstackexample.view.CView
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    val TAG = "MainActivityTest"
+
+    @Test fun showsDefaultView() {
+        launchActivity<MainActivity>().use { scenario ->
+            scenario.onActivity { activity: BackstackActivity -> run {
+                val bsActivity: BackstackActivity = activity
+                assertEquals(1, bsActivity.getCurrentViewClasses().size)
+                assertEquals(AView::class, bsActivity.getMostRecentView()::class)
+            } }
+        }
+    }
+
+    @Test fun newViewsAreAdded() {
+        var backstack: List<String>
+        launchActivity<MainActivity>().use { scenario ->
+
+            scenario.onActivity { activity: BackstackActivity -> run {
+                val bsActivity: BackstackActivity = activity
+                bsActivity.goTo(BView())
+                assertEquals(BView::class, bsActivity.getMostRecentView()::class)
+                bsActivity.goTo(CView())
+                assertEquals(CView::class, bsActivity.getMostRecentView()::class)
+
+                backstack = bsActivity.getCurrentViewClasses()
+                assertEquals(3, backstack.size)
+                assertEquals(CView::class, bsActivity.getMostRecentView()::class)
+
+                bsActivity.goTo(BView())
+                backstack = bsActivity.getCurrentViewClasses()
+                assertEquals(2, backstack.size)
+                assertEquals(BView::class, bsActivity.getMostRecentView()::class)
+
+                bsActivity.goTo(AView())
+                backstack = bsActivity.getCurrentViewClasses()
+                assertEquals(1, backstack.size)
+                assertEquals(AView::class, bsActivity.getMostRecentView()::class)
+            } }
+
+            // ... UI interactions
+        }
+    }
+
+    @Test fun viewsRemainsAfterOrientationChange() {
+        var backstack: List<String>
+        launchActivity<MainActivity>().use { scenario ->
+
+            scenario.onActivity { activity: BackstackActivity -> run {
+                val bsActivity: BackstackActivity = activity
+                bsActivity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                bsActivity.goTo(BView())
+                bsActivity.goTo(CView())
+                backstack = bsActivity.getCurrentViewClasses()
+                assertEquals(3, backstack.size)
+                assertEquals(CView::class, bsActivity.getMostRecentView()::class)
+
+                bsActivity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                while(bsActivity.requestedOrientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+                    // Do nothing
+                }
+                backstack = bsActivity.getCurrentViewClasses()
+                assertEquals(3, backstack.size)     /// TODO: WHY DOES THIS PASS??
+                assertEquals(CView::class, bsActivity.getMostRecentView()::class)
+
+                bsActivity.goTo(BView())
+                backstack = bsActivity.getCurrentViewClasses()
+                assertEquals(2, backstack.size)
+                assertEquals(BView::class, bsActivity.getMostRecentView()::class)
+
+                bsActivity.goTo(AView())
+                backstack = bsActivity.getCurrentViewClasses()
+                assertEquals(1, backstack.size)
+                assertEquals(AView::class, bsActivity.getMostRecentView()::class)
+            } }
+        }
+    }
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.gregorymarkthomas.backstackexample", appContext.packageName)
+    }
+}
